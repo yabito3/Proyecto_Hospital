3de8675962a48b9e1ddc04cb970a9ec8b54676a2
yabito3 <jyayag@unal.edu.co>
2025-06-21 22:34:41 -0500

    Add files via upload

---
+++ Bussiness/Validaciones.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bussiness;
+
+/**
+ *
+ * @author sebas
+ */
+public class Validaciones {
+    public boolean esAdmin(String usuario,String Contra){
+        return true;
+    }
+    public boolean esFuncionario(String usuario,String Contra){
+        return false;
+    }
+    public boolean esPaciente(String usuario,String Contra){
+        return false;
+    }
+}
---
+++ UI/Error.java
@@ -0,0 +1,19 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI;
+
+/**
+ *
+ * @author sebas
+ */
+import javax.swing.*;
+
+public class Error {
+    public void mostrarErrorIngreso(){
+        JOptionPane.showMessageDialog(null,"Datos invalidos o Usuario Inexistente. ","ERROR",JOptionPane.ERROR_MESSAGE);     
+    }
+    
+}
